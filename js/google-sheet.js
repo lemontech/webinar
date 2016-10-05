330b3798e82a94e70d4cb821ef36fc78bb579e23
Tadashi Junior Suto <xtadashix@gmail.com>
2016-10-05 17:36:41 -0300

    Update google-sheet.js

--- js/google-sheet.js
+++ js/google-sheet.js
@@ -26,7 +26,7 @@ $("#foo").submit(function(event){
     request = $.ajax({
         url: "https://script.google.com/a/macros/lemontech.com.br/s/AKfycbwqNr7iPoWbEs7oUqcZATfYZK2CyHZ6u6BxbGkmrWjdi1v3CaHt/exec",
         type: "post",
-        data: serializedData;
+        data: serializedData,
         headers: { "Access-Control-Allow-Origin" : "*" }
     });
 
