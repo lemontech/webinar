5bc3fd4c4f2396bcd5d105b0e330a62604e7148c
Tadashi Junior Suto <xtadashix@gmail.com>
2016-10-06 17:16:30 -0300

    confirmação

--- js/validar.js
+++ js/validar.js
@@ -18,5 +18,12 @@ if (email == "") {
 alert('Preencha o campo com seu email');
 form1.email.focus();
 return false;
+  
+var nome = document.getElementById("nomeid");
+  if (nome.value != "") {
+    alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+}
+
+} 
 }
 }
