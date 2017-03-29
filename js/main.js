da2bfbe6e6b39384e3984604ade2ab6dad3884e6
Tadashi Junior Suto <xtadashix@gmail.com>
2017-03-29 16:09:54 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,9 +28,9 @@ $(document).ready(function(){
 
     $('#countdown_dashboard').countDown({
         targetDate: {
-            'day':      24,
-            'month':    11,
-            'year':     2016,
+            'day':      25,
+            'month':    05,
+            'year':     2017,
             'hour':     23,
             'min':      59,
             'sec':      59,
